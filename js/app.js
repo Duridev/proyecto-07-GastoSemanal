1469143f5eea6ea0726b8544d55b89517b4908e3
duridev <duridev@outlook.com>
2024-02-21 23:38:13 -0300

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -14,18 +14,34 @@ function eventListener() {
 
 //********************* Clases ****************/
 
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
 
+}
+
+//** Instanciar **/
+let presupuesto;
+const ui = new UI;
 
 //********************* Funciones ****************/
 
 function preguntarPresupuesto() {
     const presupuestoUsuario  = prompt("¿Cuánto es tu presupuesto");
 
-    console.log(Number(presupuestoUsuario));
+    // console.log(Number(presupuestoUsuario));
 
     if(preguntarPresupuesto === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
     };
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 };
 
